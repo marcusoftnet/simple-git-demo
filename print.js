e9969f670f8e0dab97b6830639209efbbbd82a35
Marcus Hammarberg <marcusoft.net@gmail.com>
2020-04-27 14:00:30 +0200

    Update print.js

--- print.js
+++ print.js
@@ -3,4 +3,4 @@ const days = Math.round(
   (new Date().getTime() - start.getTime()) / (24 * 3600 * 1000)
 );
 
-console.log(`SPRING 20 - STOCKHOLM has been going on for ${days} days!`);
+console.log(`SPRING 20 - STOCKHOLM has been going on for ${days} day!`);
